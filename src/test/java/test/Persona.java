97fca99b425209453bae6e41e250fc4e11211c0c
santiago2904 <44620638+santiago2904@users.noreply.github.com>
2021-03-23 20:54:37 -0500

    Add files via upload

---
+++ src/test/java/test/Persona.java
@@ -0,0 +1,49 @@
+package classroom;
+
+public class Persona {
+
+    final long cedula;
+    String nombre;
+    static int totalPersonas = 0;
+    
+   Persona(){
+       cedula=0;
+       nombre="sin nombre";
+   }
+
+    Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+    
+    Persona(String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+
+    Persona(long cedula) {
+        this.cedula = cedula;
+        this.nombre = "";
+        totalPersonas++;
+    }
+
+    Persona(String nombre) {
+        this.nombre = "";
+        totalPersonas++;
+        cedula=1;
+    }
+    
+    long getCedula() {
+        return cedula;
+    }
+
+    String getNombre() {
+        return nombre;
+    }
+
+    void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
